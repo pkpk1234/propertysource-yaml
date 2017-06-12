6acdb68b1b10a20eec1e360615be3e03d7dcd762
lijiaming <lijiaming1984@gmail.com>
2017-06-12 21:28:39 +0800

    add unit test

--- src/test/java/com/example/propertysource/yaml/ApplicationTests.java
+++ src/test/java/com/example/propertysource/yaml/ApplicationTests.java
@@ -1,19 +1,31 @@
 package com.example.propertysource.yaml;
 
+import com.example.propertysource.yaml.controller.MessageController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.net.URI;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
-@WebMvcTest
+@WebMvcTest(MessageController.class)
 public class ApplicationTests {
+    @Autowired
+    private MockMvc mockMvc;
 
-	@Test
-	public void contextLoads() {
+    @Test
+    public void contextLoads() throws Exception {
+         mockMvc.perform(MockMvcRequestBuilders.get(URI.create("/message")).accept(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("hello yaml"));
 
-	}
+    }
 
 }
